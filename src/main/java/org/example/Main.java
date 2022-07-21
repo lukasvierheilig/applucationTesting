56e044313dfc858bdc435911c14ed9797d9b8762
lukas vierheilig <33054587+lukasvierheilig@users.noreply.github.com>
2022-07-21 20:59:06 +0200

    Update Main.java
    Edits

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        String hello = "hello";
 
 
         TestHelper testHelper = new TestHelper(new TestHelper2());
